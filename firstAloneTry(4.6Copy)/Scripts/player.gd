7d11b5a9ca38c8abe2072eca69b4df136e2e9ff6
Siltag <ihsanalp9@gmail.com>
2026-02-20 07:37:28 +0300

    Save before Stats
    Some stat systems will be changed

--- firstAloneTry(4.6Copy)/Scripts/player.gd
+++ firstAloneTry(4.6Copy)/Scripts/player.gd
@@ -7,7 +7,8 @@ extends CharacterBody2D
 @onready var sword: Node2D = $Sword/blade
 @onready var physical_body: CollisionShape2D = $CollisionShape2D
 @onready var iFrames: Timer = $immunity_frames
-@export var blood: AnimatedSprite2D
+@onready var blood: AnimatedSprite2D = $blood
+
 
 
 @export var base_health : int = 100
@@ -21,17 +22,27 @@ extends CharacterBody2D
 
 
 #region Health
+var new_health: float = base_health
 var flat_bonus_health : int = 0
-var percentage_bonus_health : float = 1
 
-var max_health: float = (base_health * percentage_bonus_health) + flat_bonus_health 
-#
-# GET MAX HP FUNCTION NEEDED
-#
+var _perc: float = 1
+var percentage_bonus_health : float = 1 :
+	set(value):
+		var current_percentage: float = current_health/new_health 
+		_perc = clamp(value, 0.1, 10.0)
+		current_health = current_percentage * get_max_hp(new_health,_perc,flat_bonus_health)
+		new_health = get_max_hp(new_health,_perc,flat_bonus_health)
+	get:
+		return _perc
+
+func get_max_hp(baseHP: float, percentage: float, flat :int) -> float:
+	return (baseHP*percentage) + flat
+
+
 var _currentHP : float
 var current_health: float = 100 : 
 	set(value):
-		_currentHP = clamp(value, 0 ,max_health)
+		_currentHP = clamp(value, 0 ,get_max_hp(new_health,percentage_bonus_health,flat_bonus_health))
 		
 		print(_currentHP)
 		
@@ -69,9 +80,12 @@ func collect_orb(_exp_amount : float) -> void:
 
 
 func _ready() -> void:
-
+	print(flat_bonus_health, " ", percentage_bonus_health, " ")
+	
+	
+	
 	GameState.player = self
-	current_health = (base_health * percentage_bonus_health) + flat_bonus_health
+	current_health = get_max_hp(new_health,percentage_bonus_health,flat_bonus_health)
 	movement_input.speed = move_speed
 	damage_area.damage = damage
 	sword_animation.speed_scale = attack_speed
@@ -79,9 +93,9 @@ func _ready() -> void:
 	sword.cooldown = cooldown
 	sword.damage = damage
 
-#func _physics_process(_delta: float) -> void:
-	#if Input.is_action_just_pressed("debug"):
-		#percentage_bonus_health += 0.1
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed("debug"):
+		percentage_bonus_health += 0.1
 
 func _exit_tree() -> void:
 	if GameState.player == self:
